13a819c226605c50c433f76a54cd5efe9fe89eea
Uporold <vg.ryndin@gmail.com>
2021-09-15 01:26:35 +0300

    add importProductsFiles lambda

--- import-service/src/functions/import-products-file/index.ts
+++ import-service/src/functions/import-products-file/index.ts
@@ -1,4 +1,3 @@
-import schema from './schema';
 import { handlerPath } from '@libs/handlerResolver';
 
 export default {
@@ -6,11 +5,14 @@ export default {
   events: [
     {
       http: {
-        method: 'post',
-        path: 'hello',
+        method: 'get',
+        path: 'import',
+        cors: true,
         request: {
-          schema: {
-            'application/json': schema,
+          parameters: {
+            querystrings: {
+              name: true,
+            },
           },
         },
       },
