b0c051d717ef5ac048cc09d5be354c6bb8554f6c
Uporold <vg.ryndin@gmail.com>
2021-09-15 01:27:43 +0300

    update serverless.ts

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,6 +1,7 @@
 import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+import dotenv from 'dotenv';
+dotenv.config();
+import importProductsFile from '@functions/import-products-file';
 
 const serverlessConfiguration: AWS = {
   service: 'import-service',
@@ -11,21 +12,76 @@ const serverlessConfiguration: AWS = {
       includeModules: true,
     },
   },
+  useDotenv: true,
   plugins: ['serverless-webpack'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
+    region: 'eu-west-1',
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      BUCKET_NAME: process.env.BUCKET_NAME,
     },
     lambdaHashingVersion: '20201221',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 's3:ListBucket',
+        Resource: `arn:aws:s3:::${process.env.BUCKET_NAME}`,
+      },
+      {
+        Effect: 'Allow',
+        Action: 's3:*',
+        Resource: `arn:aws:s3:::${process.env.BUCKET_NAME}/*`,
+      },
+    ],
   },
+  resources: {
+    Resources: {
+      CsvImportBucket: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: process.env.BUCKET_NAME,
+          AccessControl: 'Private',
+          CorsConfiguration: {
+            CorsRules: [
+              {
+                AllowedHeaders: ['*'],
+                AllowedMethods: ['GET', 'PUT', 'POST', 'DELETE', 'HEAD'],
+                AllowedOrigins: ['*'],
+              },
+            ],
+          },
+        },
+      },
+      CsvImportBucketPolicy: {
+        Type: 'AWS::S3::BucketPolicy',
+        Properties: {
+          Bucket: {
+            Ref: 'CsvImportBucket',
+          },
+          PolicyDocument: {
+            Statement: {
+              Sid: 'AllowPublicRead',
+              Effect: 'Allow',
+              Action: 's3:GetObject',
+              Resource: `arn:aws:s3:::${process.env.BUCKET_NAME}/*`,
+              Principal: {
+                AWS: '*',
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
   // import the function via paths
-  functions: { hello },
+  functions: { importProductsFile },
 };
 
 module.exports = serverlessConfiguration;
