5a41770ee90fa351e9a30259e395dadfedea0145
Uporold <vg.ryndin@gmail.com>
2021-09-14 19:07:37 +0300

    fix eslint

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -1,40 +1,40 @@
-import type { AWS } from '@serverless/typescript';
+import type { AWS } from "@serverless/typescript";
 import {
   getProductsList,
   getProductsById,
   createProduct,
-} from '@functions/index';
-import dotenv from 'dotenv';
+} from "@functions/index";
+import dotenv from "dotenv";
 dotenv.config();
 
 const serverlessConfiguration: AWS = {
-  service: 'product-service',
-  frameworkVersion: '2',
+  service: "product-service",
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: { forceInclude: ['pg'] },
+      webpackConfig: "./webpack.config.js",
+      includeModules: { forceInclude: ["pg"] },
     },
   },
   useDotenv: true,
-  plugins: ['serverless-webpack', 'serverless-offline'],
+  plugins: ["serverless-webpack", "serverless-offline"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    region: 'eu-west-1',
+    name: "aws",
+    runtime: "nodejs14.x",
+    region: "eu-west-1",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       PG_HOST: process.env.DB_HOST,
       PG_PORT: process.env.DB_PORT,
       PG_DATABASE: process.env.DB_NAME,
       PG_USERNAME: process.env.DB_USERNAME,
       PG_PASSWORD: process.env.DB_PASSWORD,
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
   },
   // import the function via paths
   functions: { getProductsList, getProductsById, createProduct },
